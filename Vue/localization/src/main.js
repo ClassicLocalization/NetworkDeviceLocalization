2f5e16b1b6ffd55d02ca6d6848ae82d7e7ab9b20
Alex <alex_v_schaefer@t-online.de>
2022-07-25 13:17:47 +0200

    added new data sets

--- Vue/localization/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueSocketio from 'vue-socket.io';
-
-
-createApp(App).mount('#app')
-this.use(VueSocketio, 'http://localhost:4000')
